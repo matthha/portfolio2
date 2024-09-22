eae988f67d8d54a3cf07472aaa5d3ee1b1dffbde
Matthew <matthha@umich.edu>
2024-09-22 13:13:38 -0500

    Create Contact and Error Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import App from './App.tsx'
+import Root from './routes/root.tsx';
 import './index.css'
+import ErrorPage from './error-page.tsx';
+import Contact from './routes/contact.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </StrictMode>,
 )
