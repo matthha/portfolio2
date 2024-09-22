d76f7dc98f5bdb2f18798ab885ddf323ac3df344
Matthew <matthha@umich.edu>
2024-09-22 14:21:27 -0500

    Add Edit page and Save data with routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,30 @@ import {
   RouterProvider,
 } from "react-router-dom";
 // import App from './App.tsx'
-import Root from './routes/root.tsx';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root.tsx';
 import './index.css'
 import ErrorPage from './error-page.tsx';
-import Contact from './routes/contact.tsx';
+import Contact, { loader as contactLoader, } from './routes/contact.tsx';
+import EditContact, { action as editAction }from "./routes/edit";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ]
   },
