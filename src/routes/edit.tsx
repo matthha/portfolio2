c1ff6fbc8b3869c16a3c6ee9298fe44f337a9a1f
Matthew <matthha@umich.edu>
2024-09-24 14:00:48 -0500

    Finish Paths

--- src/routes/edit.tsx
+++ src/routes/edit.tsx
@@ -6,7 +6,7 @@ export async function action({ request, params }) {
    const formData = await request.formData();
    const updates = Object.fromEntries(formData);
    await updateContact(params.contactId, updates);
-   return redirect(`portfolio2/contacts/${params.contactId}`);
+   return redirect(`/portfolio2/contacts/${params.contactId}`);
  }
 
 export default function EditContact() {
@@ -62,7 +62,7 @@ export default function EditContact() {
       <p>
         <button type="submit">Save</button>
         <button type="button" onClick={() => {
-            navigate('portfolio2/');
+            navigate('/portfolio2/');
           }}>Cancel</button>
       </p>
     </Form>
