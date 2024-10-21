03b5853eb96f9769959164846a07a6dcad435ef4
Matthew <matthha@umich.edu>
2024-10-21 17:32:19 -0500

    Update: Change About content

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -3,7 +3,35 @@ export default function About() {
    return (
       <div className="element-content">
          <h1>About Me</h1>
-         <div>Socials</div>
-         <p>I was always interested in tech and understanding how things work. My first website was about Scooby Doo that I made in Saturday School during middle school.</p>
+         <p>Formally trained agile UX professional, I've refined my skills as a liaison to be the bridge between problems and solutions.</p>
+
+         <h2 className=''>Socials</h2>
+         <div>
+            <a type="button" className="btn" href='https://www.linkedin.com/in/matthew-hall-87a2b9269/' target='blank'>LinkedIn</a>{'   '}
+            <a type="button" className="btn" href='https://github.com/matthha' target='blank'>Github</a>
+         </div>
+         
+         <h2>Education</h2>
+         <p>Master's in Information, Bachelor's in Human Services</p>
+         
+         <h2>Sports</h2>
+         <p>Volleyball, Tennis</p>
+         
+         <h2>Hobbies</h2>
+         <p>Crochet, Game Development</p>
+
+         <h2>Shows</h2>
+         <p>Parks and Rec, Community</p>
+  
+
+         <h2>Books</h2>
+         <p>Quantum, Scale, Claws, Freddy and Frederika</p>
+
+         <h2>Videogames</h2>
+         <p>The Talos Principle</p>
+         <p>Dark Cloud</p>
+
+
+
       </div>
    )}
